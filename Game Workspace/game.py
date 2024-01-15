ba805c3b3e8d404ee867342b9dcdc67f5634484f
AbelTogn <abeltognonipro@gmail.com>
2024-01-15 08:49:07 +0100

    modified: Game Workspace/game.py

--- Game Workspace/game.py
+++ Game Workspace/game.py
@@ -3,11 +3,11 @@ import pygame
 def main():
     pygame.init()
 
-    screen = pygame.display.set_mode((500, 500))
+    screen = pygame.display.set_mode((500, 500), pygame.RESIZABLE)
 
+    circle_x = 255
+    circle_y = 255
 
-    screen.fill((255, 255, 255))
-    pygame.display.flip()
 
     running = True
     while running:
@@ -16,9 +16,20 @@ def main():
                 running = False
 
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    pygame.draw.circle(screen, (255, 0, 0), (255, 255), 100)
-                    pygame.display.flip()
+                if event.key == pygame.K_RIGHT:
+                    circle_x += 10
+                elif event.key == pygame.K_LEFT:
+                    circle_x -= 10
+                elif event.key == pygame.K_UP:
+                    circle_y -= 10
+                elif event.key == pygame.K_DOWN:
+                    circle_y += 10
+
+        screen.fill((255, 255, 255))
+
+        pygame.draw.circle(screen, (255,0,0), (circle_x, circle_y),100)
+        pygame.display.flip()
+        
 
 
 if __name__ == "__main__":
