cb6094d962a881f470327456c29596df0087d2b4
AbelTogn <abeltognonipro@gmail.com>
2024-01-15 07:57:14 +0100

    modified: Game Workspace/game.py

--- Game Workspace/game.py
+++ Game Workspace/game.py
@@ -7,14 +7,20 @@ def main():
     # icon = pygame.image.load("icon.png")
     # pygame.display.set_icon(icon)
 
-    screen = pygame.display.set_mode((240,180))
+    screen = pygame.display.set_mode((500,500))
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.type == pygame.K_LEFT:
+                    pygame.draw.circle(screen, (255,0,0), (255,255), 100)
 
-            
+        screen.fill((255,255,255))
+
 if __name__ == "__main__":
     main()
