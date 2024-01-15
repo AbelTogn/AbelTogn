3614d2d6cc14ec25921936637c1bc1dc80b6a726
AbelTogn <abeltognonipro@gmail.com>
2024-01-15 08:58:03 +0100

    modified: Game Workspace/game.py

--- Game Workspace/game.py
+++ Game Workspace/game.py
@@ -1,13 +1,24 @@
 import pygame
+import sys
 
+class MySprite(pygame.sprite.Sprite):
+    def __init__(self, x, y, image_path):
+        super().__init__()
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((500, 500), pygame.RESIZABLE)
 
-    circle_x = 255
-    circle_y = 255
+    x, y = 255, 255
 
+    image_path = "images/sprite.png"
+
+    my_sprite = MySprite(x,y,image_path)
+    all_sprites = pygame.sprite.Group(my_sprite)
 
     running = True
     while running:
@@ -17,20 +28,24 @@ def main():
 
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    circle_x += 10
+                    x += 10
                 elif event.key == pygame.K_LEFT:
-                    circle_x -= 10
+                    x -= 10
                 elif event.key == pygame.K_UP:
-                    circle_y -= 10
+                    y -= 10
                 elif event.key == pygame.K_DOWN:
-                    circle_y += 10
+                    y += 10
 
         screen.fill((255, 255, 255))
 
-        pygame.draw.circle(screen, (255,0,0), (circle_x, circle_y),100)
+        all_sprites.update()
+
+        all_sprites.draw(screen)
+        pygame.draw.circle(screen, (255,0,0), (x, y),100)
         pygame.display.flip()
         
-
+    pygame.quit()
+    sys.exit()
 
 if __name__ == "__main__":
     main()
