9acb679f38df4c9ecbf30763b8b0bb9cecef0694
AbelTogn <abeltognonipro@gmail.com>
2024-01-15 08:35:00 +0100

    modified: Game Workspace/game.py

--- Game Workspace/game.py
+++ Game Workspace/game.py
@@ -3,26 +3,21 @@ import pygame
 def main():
     pygame.init()
 
-    # Ajouter une icône
-    # icon = pygame.image.load("icon.png")
-    # pygame.display.set_icon(icon)
-
-    screen = pygame.display.set_mode((500,500))
+    screen = pygame.display.set_mode((500, 500))
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
+
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    pygame.draw.circle(screen, (255,0,0), (255,255), 100)
+                    pygame.draw.circle(screen, (255, 0, 0), (255, 255), 100)
                     pygame.display.flip()
 
-        screen.fill((255,255,255))
+        screen.fill((255, 255, 255))
         pygame.display.flip()
-        
+
 if __name__ == "__main__":
     main()
