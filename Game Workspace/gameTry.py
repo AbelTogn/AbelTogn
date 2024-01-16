0fd692ff52865733d76185d803de7666e51b9a17
AbelTogn <abeltognonipro@gmail.com>
2024-01-16 13:08:03 +0100

    new file:   Game Workspace/gameTry.py

---
+++ Game Workspace/gameTry.py
@@ -0,0 +1,17 @@
+import pygame
+
+
+def main():
+    
+    pygame.init()
+
+    screen = pygame.display.set_mode((500,500), pygame.RESIZABLE)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+if __name__ == "__main__":
+    main()
